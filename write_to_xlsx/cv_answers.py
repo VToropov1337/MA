51218c4cd17fc1987ffa7a81a1e1956d0bb30393
VToropov1337 <vladimirtoropov87@gmail.com>
2018-09-09 23:35:32 +0300

    save

--- write_to_xlsx/cv_answers.py
+++ write_to_xlsx/cv_answers.py
@@ -19,49 +19,168 @@ WHERE reports.state = 'accepted' \
     AND cv_responses.state = 'completed' \
     AND cv_responses.req_type = 'get_bulk_result_detect_classify_images';")
 
-arr = list()
-
+data = list()
 for i in cursor:
-    arr.append(i)
+    if i[0]:
+        data.append(i)
 
 
 book = Workbook()
 sheet = book.active
+sheet['A1'] = "report_price"
+sheet['B1'] = "cv_price"
+sheet['C1'] = "geo_object"
+sheet['D1'] = "isequal"
+sheet['E1'] = "url"
 
 
-print(arr[0][1]['images'][0]['tags'][0]['price_rub']['text'])
+
+
 count = 0
-for i in arr:
+for i in data:
     count += 1
-    if i[1]['images'][0]['tags'] != []: #если есть таги, идем дальше
-        if i[1]['images'][0]['tags'][0]['price_rub'] != None: #eсли цена не нан
-            if i[1]['images'][0]['tags'][0]['price_kop'] != None:
-                print('хорошо, есть полная цена')
-                z = i[0]
-                sheet.cell(row=count, column=1).value = z
-                k = str(i[1]['images'][0]['tags'][0]['price_rub']['text']+','+i[1]['images'][0]['tags'][0]['price_kop']['text'])
-                sheet.cell(row=count, column=2).value = k
-                sheet.cell(row=count, column=3).value = i[2]
+    print(count)
+    if i[1]['images'][0]['tags'] == []:
+        url = (i[1]['images'][0]['url'])
+        sheet.cell(row=count+1, column=5).value = url
+        sheet.cell(row=count+1, column=2).value = 'Нет ответа'
+        sheet.cell(row=count+1, column=4).value = 0
+        sheet.cell(row=count+1, column=1).value = i[0]
+        sheet.cell(row=count+1, column=3).value = i[2]
+
+    elif len(i[1]['images'][0]['tags']) == 1:
+            if i[1]['images'][0]['tags'][0]['price_rub'] != None and i[1]['images'][0]['tags'][0]['price_kop'] != None:
+                r = str(i[1]['images'][0]['tags'][0]['price_rub']['text']).strip()
+                k = str(i[1]['images'][0]['tags'][0]['price_kop']['text']).strip()
+                p = str(r+'.'+k).strip()
+                p = p.strip()
+                # sheet.cell(row=count+1, column=1).value = i[0]
+                # sheet.cell(row=count+1, column=2).value = p
+                # sheet.cell(row=count+1, column=3).value = i[2]
+                if i[0] == float(p):
+                    #print('YES')
+                    sheet.cell(row=count+1, column=4).value = 1
+                    sheet.cell(row=count+1, column=1).value = i[0]
+                    sheet.cell(row=count+1, column=2).value = p
+                    sheet.cell(row=count+1, column=3).value = i[2]
+                else:
+                    #print("NO")
+                    url = (i[1]['images'][0]['url'])
+                    sheet.cell(row=count+1, column=4).value = 0
+                    sheet.cell(row=count+1, column=5).value = url
+                    sheet.cell(row=count+1, column=1).value = i[0]
+                    sheet.cell(row=count+1, column=2).value = p
+                    sheet.cell(row=count+1, column=3).value = i[2]
+            elif i[1]['images'][0]['tags'][0]['price_rub'] != None and i[1]['images'][0]['tags'][0]['price_rub']['text'] != '' and i[1]['images'][0]['tags'][0]['price_kop'] == None:
+                r = str(i[1]['images'][0]['tags'][0]['price_rub']['text']).strip()
+                if i[0] == float(r):
+                    sheet.cell(row=count+1, column=4).value = 1
+                    sheet.cell(row=count+1, column=1).value = i[0]
+                    sheet.cell(row=count+1, column=2).value = r
+                    sheet.cell(row=count+1, column=3).value = i[2]
+                else:
+                    url = (i[1]['images'][0]['url'])
+                    sheet.cell(row=count+1, column=4).value = 0
+                    sheet.cell(row=count+1, column=5).value = url
+                    sheet.cell(row=count+1, column=1).value = i[0]
+                    sheet.cell(row=count+1, column=2).value = r
+                    sheet.cell(row=count+1, column=3).value = i[2]
+            elif i[1]['images'][0]['tags'][0]['price_rub'] == None and i[1]['images'][0]['tags'][0]['price_kop'] != None:
+                url = (i[1]['images'][0]['url'])
+                sheet.cell(row=count+1, column=4).value = 0
+                sheet.cell(row=count+1, column=5).value = url
+                sheet.cell(row=count+1, column=1).value = i[0]
+                sheet.cell(row=count+1, column=2).value = str(i[1]['images'][0]['tags'][0]['price_kop']['text'])
+                sheet.cell(row=count+1, column=3).value = i[2]
+                #sheet.cell(row=count+1, column=6).value = 'только копейки'
+            else:
+                url = (i[1]['images'][0]['url'])
+                sheet.cell(row=count+1, column=4).value = 0
+                sheet.cell(row=count+1, column=5).value = url
+                sheet.cell(row=count+1, column=1).value = i[0]
+                sheet.cell(row=count+1, column=2).value = str(i[1]['images'][0]['tags'][0])
+                sheet.cell(row=count+1, column=3).value = i[2]
+                #sheet.cell(row=count+1, column=6).value = 'нет цен вообще'
+    else:
+        doublearr = list()
+        for j in i[1]['images'][0]['tags']:
+            if j['price_rub'] != None and j['price_kop'] != None:
+                r = str(j['price_rub']['text'])
+                k = str(j['price_kop']['text'])
+                p = str(r+'.'+k).strip()
+                if i[0] == float(p):
+                    url = (i[1]['images'][0]['url'])
+                    #sheet.cell(row=count+1, column=5).value = url
+                    sheet.cell(row=count+1, column=4).value = 1
+                    sheet.cell(row=count+1, column=1).value = i[0]
+                    sheet.cell(row=count+1, column=2).value = p
+                    sheet.cell(row=count+1, column=3).value = i[2]
+                    #sheet.cell(row=count+1, column=6).value = 'первая цена совпала'
+                    #sheet.cell(row=count+1, column=7).value = '>= 2 ответа'
+                    break
+                else:
+                    doublearr.append(p)
+                    url = (i[1]['images'][0]['url'])
+                    sheet.cell(row=count+1, column=5).value = url
+                    sheet.cell(row=count+1, column=4).value = 0
+                    sheet.cell(row=count+1, column=1).value = i[0]
+                    sheet.cell(row=count+1, column=2).value = str(doublearr)
+                    sheet.cell(row=count+1, column=3).value = i[2]
+                    #sheet.cell(row=count+1, column=6).value = '2 и более значения и ничего не совпало'
+                    #sheet.cell(row=count+1, column=7).value = '>= 2 ответа'
+
+
+            elif j['price_rub'] != None  and j['price_rub']['text'] != '' and j['price_kop'] == None:
+                r = str(j['price_rub']['text']).strip()
+                doublearr.append(r)
+                if i[0] == float(r):
+                    sheet.cell(row=count+1, column=4).value = 0
+                    url = (i[1]['images'][0]['url'])
+                    sheet.cell(row=count+1, column=5).value = url
+                    sheet.cell(row=count+1, column=1).value = i[0]
+                    sheet.cell(row=count+1, column=2).value = str(doublearr)
+                    sheet.cell(row=count+1, column=3).value = i[2]
+                    #sheet.cell(row=count+1, column=6).value = 'совпали только рубли'
+                    #sheet.cell(row=count+1, column=7).value = '>= 2 ответа'
+                    #sheet.cell(row=count+1, column=8).value = str(i[1]['images'][0]['tags'])
+                    break
+                else:
+                    doublearr.append(r)
+                    url = (i[1]['images'][0]['url'])
+                    sheet.cell(row=count+1, column=4).value = 0
+                    sheet.cell(row=count+1, column=5).value = url
+                    sheet.cell(row=count+1, column=1).value = i[0]
+                    sheet.cell(row=count+1, column=2).value = str(doublearr)
+                    sheet.cell(row=count+1, column=3).value = i[2]
+                    #sheet.cell(row=count+1, column=6).value = 'должно быть 2 значения и только копейки'
+                    #sheet.cell(row=count+1, column=7).value = '>= 2 ответа'
 
             else:
-                print('неплохо, есть только цена')
-                z = i[0]
-                sheet.cell(row=count, column=1).value = z
-                k = str(i[1]['images'][0]['tags'][0]['price_rub']['text'])
-                sheet.cell(row=count, column=2).value = k
-                sheet.cell(row=count, column=3).value = i[2]
+                if j['price_rub'] and j['price_rub']['text'] != '':
+                    r = str(j['price_kop']['text']).strip()
+                else:
+                    r = "-"
+                if j['price_kop'] and j['price_kop']['text'] != '':
+                    k = str(j['price_kop']['text']).strip()
+                else:
+                    k = "-"
+                p = str(r+'.'+k)
+                doublearr.append(p)
+                #sheet.cell(row=count+1, column=6).value = 'хуйня какая-то'
+                sheet.cell(row=count+1, column=1).value = i[0]
+                url = (i[1]['images'][0]['url'])
+                sheet.cell(row=count+1, column=2).value = str(doublearr)
+                sheet.cell(row=count+1, column=3).value = i[2]
+                sheet.cell(row=count+1, column=4).value = 0
+                sheet.cell(row=count+1, column=5).value = url
+                #sheet.cell(row=count+1, column=7).value = '>= 2 ответа'
+                #sheet.cell(row=count+1, column=8).value = str(i[1]['images'][0]['tags'])
 
-        else:
-            print('цена отсутствует')
-            z = i[0]
-            sheet.cell(row=count, column=1).value = z
-            sheet.cell(row=count, column=3).value = i[2]
-    else:
-        print('плохо')
-        z = i[0]
-        sheet.cell(row=count, column=1).value = z
-        sheet.cell(row=count, column=3).value = i[2]
 
 
-book.save('prices.xlsx')
+
+
+
+
+book.save('cv_answers.xlsx')
 connect.close()
