df38925430e1cbf5152a085313b335302d6fbd09
VToropov1337 <vladimirtoropov87@gmail.com>
2018-09-28 16:15:41 +0300

    save

---
+++ cv/json_modify/old-new2.py
@@ -0,0 +1,46 @@
+import json
+import pprint
+
+old = list()
+new = list()
+
+false = False
+true = True
+
+
+
+with open('auchan_old.json','r') as f2:
+    old = json.loads(f2.read())
+
+
+
+
+
+for i in old:
+    c = 0
+    c1 = 1
+    hh = dict()
+    hh['data'] = ''
+    hh['objects'] = list()
+    hh['result'] = ''
+    hh['source'] = i['source']
+    for j in i['predicted_objects']:
+        wrapped = dict()
+        polygon = dict()
+        wrapped['class'] = j['class']
+        wrapped['class_id'] = c
+        wrapped['modified'] = i['modified']
+        polygon['id'] = c
+        polygon['markup_id'] = c+1
+        polygon['vertexes'] = j['polygons'][0]['points']
+        wrapped['polygon'] = polygon
+        c += 1
+        hh['objects'].append(wrapped)
+        
+    new.append(hh)
+
+
+
+
+with open('auchan-new.json','w') as nf:
+    nf.write(json.dumps(new))
---
+++ cv/json_modify/old_new.py
@@ -0,0 +1,39 @@
+import json
+import pprint
+
+old = list()
+new = list()
+false = False
+true = True
+
+
+
+with open('magnit.json','r') as f2:
+    old = json.loads(f2.read())
+
+
+c = 0
+for i in old:
+    hh = dict()
+    hh['data'] = ''
+    hh['class'] = ''
+    hh['objects'] = list()
+    for j in i['polygons']:
+        smpl = dict()
+        polygon = dict()
+        smpl['modified'] = false
+        smpl['polygon'] = dict()
+        smpl['polygon']['id'] = j['id']
+        smpl['polygon']['vector'] = []
+        smpl['polygon']['vertexes'] = j['vertexes']
+        hh['objects'].append(smpl)
+
+    hh['result'] = ''
+    hh['source'] = i['source']
+    new.append(hh)
+
+
+
+
+with open('magnit-new.json','w') as nf:
+    nf.write(json.dumps(new))
