065825f65bf35d81ff8ca924e4b2c6e8e5d3a4c4
VToropov1337 <vladimirtoropov87@gmail.com>
2019-01-17 23:58:42 +0300

    save

--- begin_mongo_db/introduce.py
+++ begin_mongo_db/introduce.py
@@ -32,7 +32,7 @@ post_data = {
     'author': 'Scott'
 }
 
-
+#insers a value
 result = posts.insert_one(post_data)
 print('One post: {0}'.format(result.inserted_id))
 
@@ -40,3 +40,38 @@ print('One post: {0}'.format(result.inserted_id))
 
 #find a data
 posts.find_one({'author': 'Scott'})
+
+
+
+#insert a many values
+post_1 = {
+    'title': 'Python and MongoDB',
+    'content': 'PyMongo is fun, you guys',
+    'author': 'Scott'
+}
+post_2 = {
+    'title': 'Virtual Environments',
+    'content': 'Use virtual environments, you guys',
+    'author': 'Scott'
+}
+post_3 = {
+    'title': 'Learning Python',
+    'content': 'Learn Python, it is easy',
+    'author': 'Bill'
+}
+new_result = posts.insert_many([post_1, post_2, post_3])
+print('Multiple posts: {0}'.format(new_result.inserted_ids))
+
+#find a data
+bills_post = posts.find_one({'author': 'Bill'})
+print(bills_post)
+
+
+#this is a cursor object
+scotts_posts = posts.find({'author': 'Scott'})
+print(scotts_posts)
+
+#iteration 
+for post in scotts_posts:
+    print(post)
+
